0664acd793f8f0738ba80dec3bf17c18e82a7532
christian <christianjones94@gmail.com>
2022-11-30 12:57:13 -0500

    added buttons/refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,30 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const todoRef = useRef();
+  const [subTask, setSubTask] = useState([]);
+  const subTaskRef = useRef();
+  const taskRef = useRef();
+  const [task, setTask] = useState([]);
+  let currentRef = [taskRef, subTaskRef];
 
   return (
     <div className="App">
-      <input type="text" ref={todoRef} />
-      <button onClick={() => setTodoList([...todoList, todoRef.current.value])}>
+      <input type="text" ref={currentRef[0]} />
+      <button onClick={() => setTask([...task, taskRef.current.value])}>
+        Add List
+      </button>
+      <button
+        onClick={() => setSubTask([...subTask, subTaskRef.current.value])}
+      >
         Add todo
       </button>
       <div>
-        {todoList.map(function (todoRef, index) {
+        {task.map(function (taskRef, index) {
+          return <button>{taskRef}</button>;
+        })}
+      </div>
+      <div>
+        {subTask.map(function (todoRef, index) {
           return <li>{todoRef}</li>;
         })}
       </div>
